72450926415089c3f0668a49bb77ce53f85f71a1
jrafa <dj.czosnek@gmail.com>
2017-11-15 10:35:40 +0100

    add state

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -1,39 +1,62 @@
 package com.example.android.scorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String STATE_SCORE_A = "scoreA";
+    private static final String STATE_SCORE_B = "scoreB";
     private int scoreTeamA = 0;
     private int scoreTeamB = 0;
+    private TextView counterTeamA;
+    private TextView counterTeamB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            scoreTeamA = savedInstanceState.getInt(STATE_SCORE_A);
+            scoreTeamB = savedInstanceState.getInt(STATE_SCORE_B);
+        }
+
+        counterTeamA = (TextView) findViewById(R.id.team_a_score);
+        counterTeamA.setText(Integer.toString(scoreTeamA));
+
+        counterTeamB = (TextView) findViewById(R.id.team_b_score);
+        counterTeamB.setText(Integer.toString(scoreTeamB));
+
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(STATE_SCORE_A, scoreTeamA);
+        outState.putInt(STATE_SCORE_B, scoreTeamB);
     }
 
     /**
-     *  Display points for team A.
+     * Display points for team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+        counterTeamA.setText(String.valueOf(score));
     }
 
     /**
-     *  Display points for team B.
+     * Display points for team B.
      */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
+        counterTeamB.setText(String.valueOf(score));
     }
 
     /**
-     *  Increase score team A by 6 points.
+     * Increase score team A by 6 points.
      */
     public void addSixPointsTeamA(View view) {
         scoreTeamA += 6;
@@ -41,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *  Increase score team A by 3 points.
+     * Increase score team A by 3 points.
      */
     public void addThreePointsTeamA(View view) {
         scoreTeamA += 3;
@@ -49,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *  Increase score team A by 2 points.
+     * Increase score team A by 2 points.
      */
     public void addTwoPointsTeamA(View view) {
         scoreTeamA += 2;
@@ -57,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *  Increase score team A by 1 point.
+     * Increase score team A by 1 point.
      */
     public void addOnePointTeamA(View view) {
         scoreTeamA += 1;
@@ -65,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *  Increase score team B by 6 points.
+     * Increase score team B by 6 points.
      */
     public void addSixPointsTeamB(View view) {
         scoreTeamB += 6;
@@ -73,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *  Increase score team B by 3 points.
+     * Increase score team B by 3 points.
      */
     public void addThreePointsTeamB(View view) {
         scoreTeamB += 3;
@@ -81,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *  Increase score team B by 2 points.
+     * Increase score team B by 2 points.
      */
     public void addTwoPointsTeamB(View view) {
         scoreTeamB += 2;
@@ -89,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *  Increase score team B by 1 point.
+     * Increase score team B by 1 point.
      */
     public void addOnePointTeamB(View view) {
         scoreTeamB += 1;
@@ -97,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *   Reset points in team A and B.
+     * Reset points in team A and B.
      */
     public void reset(View view) {
         scoreTeamA = scoreTeamB = 0;
